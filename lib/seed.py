ae580a1b8a462cd3dac684e53322ded2bd807855
Dylan Tin <dylant711@gmail.com>
2024-10-23 19:45:17 -0400

    final touches

--- lib/seed.py
+++ lib/seed.py
@@ -9,14 +9,20 @@ def seed_database():
     Animal.create_table()
     Rating.create_table()
 
+    turtle = Animal.create("Grandpa", "Turtle")
+    bird = Animal.create("Toby", "Bird")
     dog = Animal.create("Jolly", "Dog")
     cat = Animal.create("Felix", "Cat")
+    horse = Animal.create("Stalwart", "Horse")
     capybara = Animal.create("Cheesecake", "Capybara")
 
+    Rating.create(bird.id, 4, "This bird talks back, kinda cool!")
     Rating.create(dog.id, 4, "Such a friendly animal!")
     Rating.create(cat.id, 3, "Very cute but it did scratch me...")
+    Rating.create(horse.id, 5, "What a beautiful mane! I would love to look after him.")
     Rating.create(capybara.id, 5, "So cute!")
     Rating.create(capybara.id, 5, "It's so fun to watch her eat grass!")
+    Rating.create(turtle.id, 2, "Nice shell, but it doesn't really do anything.")
 
 
 seed_database()
